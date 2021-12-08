3dcdb1d97a3cdbb0a7b2bc88a143d92697527c20
wangning <17865565575@163.com>
2021-12-08 17:42:05 +0800

    常规提交

--- src/test/java/AutowiredAndResourceTest.java
+++ src/test/java/AutowiredAndResourceTest.java
@@ -1,5 +1,17 @@
+import com.wn.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Resource;
+
 /**
  * @author 王宁 2021/12/8
  */
 public class AutowiredAndResourceTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserService userService2;
+    @Resource
+    UserService userService3;
+
 }
