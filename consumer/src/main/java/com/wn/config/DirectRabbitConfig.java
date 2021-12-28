7702e6fc66a2121f0e32a171c0d6166ffa5a7030
wangning <17865565575@163.com>
2021-12-28 13:59:21 +0800

    完美整合rabbitmq到springboot

--- consumer/src/main/java/com/wn/config/DirectRabbitConfig.java
+++ consumer/src/main/java/com/wn/config/DirectRabbitConfig.java
@@ -6,38 +6,43 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+/**
+ * @Author : JCccc
+ * @CreateTime : 2019/9/3
+ * @Description :
+ **/
 @Configuration
 public class DirectRabbitConfig {
 
-    /**
-     * 交换机
-     */
+    //队列 起名：TestDirectQueue
     @Bean
-    public DirectExchange myDirectExchange() {
-        // 参数意义:
-        // name: 名称
-        // durable: true
-        // autoDelete: 自动删除
-        return new DirectExchange("ForeverDirectExchange", true, false);
+    public Queue TestDirectQueue() {
+        // durable:是否持久化,默认是false,持久化队列：会被存储在磁盘上，当消息代理重启时仍然存在，暂存队列：当前连接有效
+        // exclusive:默认也是false，只能被当前创建的连接使用，而且当连接关闭后队列即被删除。此参考优先级高于durable
+        // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
+        //   return new Queue("TestDirectQueue",true,true,false);
+
+        //一般设置一下队列的持久化就好,其余两个就是默认false
+        return new Queue("TestDirectQueue", true);
     }
 
-    /**
-     * 队列
-     */
+    //Direct交换机，起名：TestDirectExchange
     @Bean
-    public Queue myDirectQueue() {
-
-        return new Queue("ForeverDirectQueue", true);
+    DirectExchange TestDirectExchange() {
+        //  return new DirectExchange("TestDirectExchange",true,true);
+        return new DirectExchange("TestDirectExchange", true, false);
     }
 
-    /**
-     * 绑定
-     */
+    //绑定，将队列和交换机绑定，并设置用于匹配键：TestDirectRouting
     @Bean
-    public Binding bindingDirect() {
-        return BindingBuilder.bind(myDirectQueue())
-                             .to(myDirectExchange())
-                             .with("forever.direct.routing");
+    Binding bindingDirect() {
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+
+    @Bean
+    DirectExchange lonelyDirectExchange() {
+        return new DirectExchange("lonelyDirectExchange");
     }
 }
--- producer/src/test/java/RabbitMQTest.java
+++ producer/src/test/java/RabbitMQTest.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -24,7 +25,7 @@ public class RabbitMQTest {
     RabbitTemplate rabbitTemplate;
 
     @Test
-    public void Test01(){
+    public void Test01() {
         String messageId = String.valueOf(UUID.randomUUID());
         String messageId2 = String.valueOf(UUID.randomUUID());
         System.out.println(messageId);
@@ -36,10 +37,10 @@ public class RabbitMQTest {
         String messageId = String.valueOf(UUID.randomUUID());
         String messageData = "这是生产者发的一条消息。";
         String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        Map<String,Object> map=new HashMap<>();
-        map.put("messageId",messageId);
-        map.put("messageData",messageData);
-        map.put("createTime",createTime);
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
 
         //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
         rabbitTemplate.convertAndSend("OliverDirectExchange", "TestDirectRouting", map);
@@ -47,9 +48,74 @@ public class RabbitMQTest {
 
     @Test
     public void send() {
-
-        String msg = "这就是第一条测试！";
-        rabbitTemplate.convertAndSend("ForeverDirectExchange", "forever.direct.routing", msg);
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "test message, hello!";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+        rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
     }
 
-}
+    /*以下两个方法测试主题交换机*/
+    @Test
+    public void sendTopicMessage1() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "message: M A N ";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> manMap = new HashMap<>();
+        manMap.put("messageId", messageId);
+        manMap.put("messageData", messageData);
+        manMap.put("createTime", createTime);
+        rabbitTemplate.convertAndSend("topicExchange", "topic.man", manMap);
+    }
+    @Test
+    public void sendTopicMessage2() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "message: woman is all ";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> womanMap = new HashMap<>();
+        womanMap.put("messageId", messageId);
+        womanMap.put("messageData", messageData);
+        womanMap.put("createTime", createTime);
+        rabbitTemplate.convertAndSend("topicExchange", "topic.woman", womanMap);
+    }
+    /*以下一个方法测试扇形交换机*/
+    @Test
+    public void sendFanoutMessage() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "message: testFanoutMessage ";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        rabbitTemplate.convertAndSend("fanoutExchange", null, map);
+    }
+    /*没有交换机的生产反馈测试*/
+    @Test
+    public void TestMessageAck() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "message: non-existent-exchange test message ";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        rabbitTemplate.convertAndSend("non-existent-exchange", "TestDirectRouting", map);
+    }
+    /*有交换机没队列的生产反馈测试*/
+    @Test
+    public void TestMessageAck2() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "message: lonelyDirectExchange test message ";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        rabbitTemplate.convertAndSend("lonelyDirectExchange", "TestDirectRouting", map);
+    }
+    }
