5b13ac654f0b806fad79bd7a3040c2efc03c41c1
wangning <17865565575@163.com>
2022-03-07 20:22:52 +0800

    整理lazada解析base64和文件下载

--- producer/src/main/java/com/wn/controller/HtmlController.java
+++ producer/src/main/java/com/wn/controller/HtmlController.java
@@ -2,6 +2,12 @@ package com.wn.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.lazada.lazop.api.LazopClient;
+import com.lazada.lazop.api.LazopRequest;
+import com.lazada.lazop.api.LazopResponse;
+import com.lazada.lazop.util.ApiException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,7 +16,31 @@ import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("html")
 public class HtmlController {
+
+    @RequestMapping("download")
+    public Boolean downloadLazadaHtml(String filePath) throws InterruptedException, ApiException, UnsupportedEncodingException {
+
+        String appkey = "100132";
+        String appSecret = "BC9A632E5E937E6686DBE82348423DF491940AFE690F16E58AE5B6A9E59BD0A7";
+        String url = "https://api.lazada.com.my/rest";
+        String accessToken = "50000101b22qjsqHXEfpfLxVCGHNiWfRT91f4e2526E3HTtgwsaPlMy8h7EYk";
+
+        LazopClient client = new LazopClient(url, appkey, appSecret);
+        LazopRequest request = new LazopRequest();
+        request.setApiName("/order/document/awb/html/get");
+        request.setHttpMethod("GET");
+        request.addApiParameter("order_item_ids", "[324649432343475,324649432443475]");
+        LazopResponse response = client.execute(request, accessToken);
+        Thread.sleep(10);
+
+        String jsonAttribute = getJsonAttribute(response.getBody());
+
+        return downloadHtml(jsonAttribute, filePath);
+    }
+
 
     /**
      * 获取lazada返回的base64字符串
@@ -38,7 +68,7 @@ public class HtmlController {
      * @param filePath
      * @throws UnsupportedEncodingException
      */
-    public Boolean base64ToHtml(String base64String, String filePath) throws UnsupportedEncodingException {
+    public Boolean downloadHtml(String base64String, String filePath) throws UnsupportedEncodingException {
 
         // 解码base64
         final Base64.Decoder decoder = Base64.getDecoder();
--- producer/src/main/java/com/wn/controller/PdfController.java
+++ producer/src/main/java/com/wn/controller/PdfController.java
@@ -1,5 +1,7 @@
 package com.wn.controller;
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.HmacUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.Mac;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileOutputStream;
@@ -16,8 +19,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Date;
 
 /**
  * @author 王宁 2022/3/6
@@ -26,6 +35,32 @@ import java.util.Base64;
 @RequestMapping("pdf")
 public class PdfController {
 
+    public Boolean downloadLazadaPdf(String filePath, HttpServletResponse response) throws IOException, InterruptedException {
+        String aa = "27df6b30502b2fd457864fbaaced6c054c90b5a6c92e59b0dfcbe7f145588465";
+
+        long timestamp = (new Date().getTime()) / 1000;
+
+        String url = "https://partner.shopeemobile.com/api/v1/logistics/forder_waybill/get_mass";
+
+        String body = "{\"partner_id\":2003284,\"timestamp\":" + timestamp + ",\"orders_list\":[\"ordersn\":\"220228MY6DR5H5\",\"package_number\":\"# TL5547917843\"],\"shopid\":96947910}";
+
+
+        String method = "POST";
+        Mac mac = HmacUtils.getInitializedMac("HmacSHA256", aa.getBytes(StandardCharsets.UTF_8));
+        mac.reset();
+        String sign = Hex.encodeHexString(mac.doFinal((url + '|' + body).getBytes(StandardCharsets.UTF_8)));
+        HttpRequest.Builder builder = HttpRequest.newBuilder(URI.create(url));
+        HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers.ofString(body, StandardCharsets.UTF_8);
+        HttpRequest request = builder.header("Content-Type", "application/json").header("Authorization", sign)
+                .method(method, bodyPublisher).build();
+        HttpClient.Builder clientBuilder = HttpClient.newBuilder();
+        HttpClient client = clientBuilder.version(HttpClient.Version.HTTP_1_1).build();
+        HttpResponse<String> resp = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        String base64PdfUrl = getBase64PdfUrl(resp.body(), filePath, response);
+        return downloadPdf(base64PdfUrl, filePath, response);
+    }
+
     /**
      * 解码lazada GetAwbDocumentHtml接口返回到base64字符串，并获取其中的pdf文件url
      *
@@ -33,7 +68,7 @@ public class PdfController {
      * @param filePath     下载到到路径
      * @throws IOException
      */
-    public void base64ToPdf(String base64String, String filePath, HttpServletResponse response) throws IOException {
+    public String getBase64PdfUrl(String base64String, String filePath, HttpServletResponse response) throws IOException {
 
         // 解码base64
         final Base64.Decoder decoder = Base64.getDecoder();
@@ -43,17 +78,14 @@ public class PdfController {
         //得到标签内容
         Element element = doc.getElementsByTag("iframe").get(0);
         // 获取lazada返回参数的src属性的值
-        String lazadaPdfUrl = element.attr("src");
-
-        // 下载pdf文件到指定路径
-        pdfDownload(lazadaPdfUrl, filePath, response);
+        return element.attr("src");
     }
 
     /**
      * 根据pdf url，下载pdf到指定到路径
      */
     @GetMapping(value = "/download")
-    public void pdfDownload(String url, String filePath, HttpServletResponse response) {
+    public Boolean downloadPdf(String url, String filePath, HttpServletResponse response) {
 
         ServletOutputStream out = null;
         InputStream ips = null;
@@ -62,14 +94,14 @@ public class PdfController {
             pdfUrl = new URL(url);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            return;
+            return false;
         }
         HttpURLConnection uc = null;
         try {
             uc = (HttpURLConnection) pdfUrl.openConnection();
         } catch (IOException e1) {
             e1.printStackTrace();
-            return;
+            return false;
         }
         try {
             uc.setDoInput(true);//设置是否要从 URL 连接读取数据,默认为true
@@ -102,6 +134,7 @@ public class PdfController {
                 e.printStackTrace();
             }
         }
+        return true;
     }
 
     /**
