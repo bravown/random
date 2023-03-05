d4fd06914df3674e06e0cb04878cd2c737ff0936
wangning <17865565575@163.com>
2023-03-05 15:18:52 +0800

    add:初始化项目

---
+++ random-consumer/src/main/java/com/wn/ConsumerAppliaction.java
@@ -0,0 +1,17 @@
+package com.wn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王宁 2021/12/27
+ */
+@SpringBootApplication
+public class ConsumerAppliaction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerAppliaction.class, args);
+    }
+
+}
+
---
+++ random-generator/src/main/java/com/ccreate/zhicai/mp/generator/Generator.java
@@ -0,0 +1,61 @@
+package com.ccreate.zhicai.mp.generator;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class Generator {
+
+    public static void main(String[] args) {
+
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 2、全局配置
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(System.getProperty("user.dir") + "/ccreate-zhicai-mp-generator/src/main/java");
+        globalConfig.setAuthor("王宁"); // TODO 修改作者
+        globalConfig.setOpen(false);
+        globalConfig.setFileOverride(true);
+//         globalConfig.setSwagger2(true); // TODO 生成PO关闭 生成VO开启
+        mpg.setGlobalConfig(globalConfig);
+
+        // 3、数据源配置
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setUrl("jdbc:mysql://localhost:3306/db_random?characterEncoding=UTF8&serverTimezone=Asia/Shanghai");
+        dataSourceConfig.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSourceConfig.setUsername("root");
+        dataSourceConfig.setPassword("123456");
+
+        mpg.setDataSource(dataSourceConfig);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(null); //模块名
+        pc.setParent("com.ccreate.zhicai.goods");
+        pc.setController("controller");
+        pc.setEntity("entity");
+        pc.setService("service.mybatisplus");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setInclude("sys_user"); //对哪一张或多张表生成代码
+        strategy.setNaming(NamingStrategy.underline_to_camel); //数据库表映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel); //数据库表字段映射到实体的命名策略
+        strategy.setEntityLombokModel(true); // lombok 模型 @Accessors(chain = true) setter链式操作
+        strategy.setRestControllerStyle(true); //restful api风格控制器
+        strategy.setControllerMappingHyphenStyle(true); //url中驼峰转连字符
+
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+
+    }
+
+}
